9c5e96ed51b5281752d94bee214bdbdca310aba6
Alexandre Friquet <alexandre.friquet@infopiiaf.fr>
2011-05-18 16:05:06 +0200

    model and controller specs modified and fixtures removed

--- lib/generators/nifty/scaffold/templates/tests/rspec/controller.rb
+++ lib/generators/nifty/scaffold/templates/tests/rspec/controller.rb
@@ -1,8 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe <%= plural_class_name %>Controller do
-  fixtures :all
-  render_views
-
-  <%= controller_methods 'tests/rspec/actions' %>
+  
+  def mock_<%= class_name.underscore %>(stubs={})
+    @mock_<%= class_name.underscore %> ||= mock_model(<%= class_name %>, stubs).as_null_object
+  end
+  
+  context "as a guest" do
+    it_should_require_admin_for_actions :index, :show, :new, :create, :edit, :update, :destroy
+      
+    end
+  end
+  
+  # <%= controller_methods 'tests/rspec/actions' %>
 end
--- lib/generators/nifty/scaffold/templates/tests/rspec/model.rb
+++ lib/generators/nifty/scaffold/templates/tests/rspec/model.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe <%= class_name %> do
-  it "should be valid" do
-    <%= class_name %>.new.should be_valid
+  context "validations" do
+    it { should validate_presence_of :title }
   end
 end
